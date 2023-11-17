af48b6ea51d50059fa7d9cbdac2c29f171ddbb82
Yusuke <yhakamaya@adobe.com>
2023-11-17 12:46:48 +0900

    update cards.js
    replace li with sp-card

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,16 +1,23 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import '@spectrum-web-components/card/sp-card.js';
 
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+    const card = document.createElement('sp-card');
+    card.setAttribute('heading', 'Card Heading');
+    card.setAttribute('subheading', 'JPG Photo');
+    while (row.firstElementChild) {
+      const child = row.firstElementChild;
+      if (child.tagName === 'IMG') {
+        child.setAttribute('slot', 'cover-photo');
+      } else {
+        child.setAttribute('slot', 'footer');
+      }
+      card.append(child);
+    }
+    ul.append(card);
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
