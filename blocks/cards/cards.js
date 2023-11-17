ce2325afdc2058804ebf8f95c57002c7fdbf4587
Yusuke <yhakamaya@adobe.com>
2023-11-17 16:42:49 +0900

    update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,36 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
+    const strongElements = row.querySelectorAll('strong');
+    const heading = strongElements[0].textContent;
+
+    const imgElement = row.querySelector('img');
+    const imageSrc = imgElement.src;
+    const altText = imgElement.alt;
+
     const card = document.createElement('sp-card');
-    while (row.firstElementChild) card.append(row.firstElementChild);
-    [...card.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(card);
+    card.setAttribute('heading', heading);
+
+    const img = document.createElement('img');
+    img.setAttribute('slot', 'cover-photo');
+    img.setAttribute('src', imageSrc);
+    img.setAttribute('alt', altText);
+    card.appendChild(img);
+
+    const subheading = document.createElement('span');
+    subheading.setAttribute('slot', 'subheading');
+    subheading.textContent = 'Your subheading here';
+    card.appendChild(subheading);
+
+    const footer = document.createElement('div');
+    footer.setAttribute('slot', 'footer');
+    footer.textContent = 'Your footer content here';
+    card.appendChild(footer);
+
+    ul.appendChild(card);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+
   block.textContent = '';
   block.append(ul);
 }
