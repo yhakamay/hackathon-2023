e4b8232518ac6e5947d8dcb413798e4120752997
Yusuke <yhakamaya@adobe.com>
2023-11-17 13:29:32 +0900

    update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,17 +6,11 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const card = document.createElement('sp-card');
-    card.setAttribute('heading', 'Card Heading');
-    card.setAttribute('subheading', 'JPG Photo');
-    while (row.firstElementChild) {
-      const child = row.firstElementChild;
-      if (child.tagName === 'IMG') {
-        child.setAttribute('slot', 'cover-photo');
-      } else {
-        child.setAttribute('slot', 'footer');
-      }
-      card.append(child);
-    }
+    while (row.firstElementChild) card.append(row.firstElementChild);
+    [...card.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
     ul.append(card);
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
