bf8e96c49cf091ca74e9a63971e8dbe6b5c1ccdc
Yusuke <yhakamaya@adobe.com>
2023-11-17 14:50:59 +0900

    rm console.log

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,7 +2,6 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  console.log('cards.js');
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const card = document.createElement('sp-card');
